7b5cef2188ab1118b871f8124b57eabbb7e23200
Alex <alexkaasik>
2021-03-26 13:29:34 +0200

    random

---
+++ random.py
@@ -0,0 +1,27 @@
+from random import*
+
+#k=0 # doesnt have a random number
+#while True:
+ #   k+=1#
+  #  #print (f"{k}-e random number")
+   # a=randint(0,4000)
+    #print(a)
+    #if a>2000 or k>=10:
+     #   break
+
+
+###2
+##k=0 # doesnt have a random number
+##a=0
+##while a<=2000 and k<10:
+##    k+=1
+##    print (f"{k}-e random number")
+##    a=randint(0,4000)
+##    print(a)
+
+
+
+for k in range(1,11):
+    print (k)
+    a=randint(0,4000)
+    print(a)
